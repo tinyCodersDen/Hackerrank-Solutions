e4eb3195062c98d481f902676c757728f0a0ad87
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-14 16:37:21 -0400

    Create Data Types.py

---
+++ 30 Days of Code/Data Types.py
@@ -0,0 +1,21 @@
+# Link: https://www.hackerrank.com/challenges/30-data-types/problem
+# This is Day 1 of 30 Days of Code
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+x = int(input(''))
+y = float(input(''))
+z = input('')
+print(x+i)
+print(y+d)
+print(s+z)
