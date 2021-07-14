6a2537568b1423fdcfab5f7d7a704da1bd429bab
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-14 16:19:19 -0400

    Create Print Function.py

---
+++ Python/Print Function.py
@@ -0,0 +1,7 @@
+# Link: https://www.hackerrank.com/challenges/python-print/problem
+if __name__ == '__main__':
+    n = int(input())
+    string = ''
+    for y in range(1,n+1):
+        string+=str(y)
+    print(string)
