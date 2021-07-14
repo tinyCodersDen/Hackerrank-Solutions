eb49343dfc364b17cb42c31781739938d36697da
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-14 16:39:45 -0400

    Create Conditional Statements.py

---
+++ 30 Days of Code/Conditional Statements.py
@@ -0,0 +1,15 @@
+# Link: https://www.hackerrank.com/challenges/30-conditional-statements/problem
+# This is Day 3 of 30 Days of Code
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if N%2==0 and N<6 and N>20:
+        print('Not',)
+    print('Wierd')
