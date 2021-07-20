6cff80174c881d5cbb79f7c998ed61042138ddbc
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-20 16:43:26 -0400

    Create Capitalize.py

---
+++ Python/Capitalize.py
@@ -0,0 +1,24 @@
+# Link: https://www.hackerrank.com/challenges/capitalize/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    for a in s.split():
+        s = s.replace(a, a.capitalize())
+    return s
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
