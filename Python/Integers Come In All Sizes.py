20f06c603dd8758f1b38c32264cb77b951d81177
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-20 16:48:31 -0400

    Create Integers Come In All Sizes.py

---
+++ Python/Integers Come In All Sizes.py
@@ -0,0 +1,6 @@
+# Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+a = int(input(''))
+b = int(input(''))
+c = int(input(''))
+d = int(input(''))
+print(a**b+c**d)
