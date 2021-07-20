8f26698b261284ae81b8640da89c4448819cb0f7
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-20 16:46:57 -0400

    Create Set Add.py

---
+++ Python/Set Add.py
@@ -0,0 +1,7 @@
+# Link: https://www.hackerrank.com/challenges/py-set-add/problem
+num1 = int(input(''))
+set1 = set('')
+for t in range(num1):
+    ask = input('')
+    set1.add(ask)
+print(len(set1))
